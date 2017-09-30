8aa3ed25723b27f78bd0789792b34bf336cc94ec
Joshua Torres <torres_306@yahoo.com>
2017-09-30 19:55:09 -0400

    add test to restrict requests

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -25,19 +25,27 @@ class QuestionsController < ApplicationController
   end
 
   def edit
+    unless @question.user == current_user
+      flash[:alert] = "You can only edit your own question."
+      redirect_to root_path 
+    end
   end
 
   def update
-
-    if @question = Question.update(question_params)
-      flash[:success] = "Question was updated"
-      redirect_to @question
-
+    unless @question.user == current_user
+      flash[:danger] = "You can only edit your own question."
+      redirect_to root_path 
     else
-      flash.now[:error] = "Question was not updated"
-      render :edit
-    end
 
+      if @question = Question.update(question_params)
+        flash[:success] = "Question was updated"
+        redirect_to @question
+
+      else
+        flash.now[:error] = "Question was not updated"
+        render :edit
+      end
+    end
   end
 
   def destroy
--- spec/requests/questions_spec.rb
+++ spec/requests/questions_spec.rb
@@ -2,7 +2,8 @@ RSpec.describe "Questions", type: :request do
   before do
 
     @user = FactoryGirl.create(:user)
-    login_as(@user)
+    @user2 = FactoryGirl.create(:user)
+    # login_as(@user)
 
     @question = FactoryGirl.create(:question, user: @user )
   end
@@ -26,4 +27,42 @@ RSpec.describe "Questions", type: :request do
       end 
     end
   end 
+
+  describe 'GET /questions/:id/edit' do 
+
+    context 'with non-signed in user' do
+      before { get "/questions/#{@question.id}/edit" }
+      
+      it "redirects to the signin page" do 
+        expect(response.status).to eq 302
+        flash_message = "You need to sign in or sign up before continuing."
+        expect(flash[:alert]).to eq flash_message
+      end 
+    end
+
+    context 'with logged in non-owners' do 
+      before do
+        login_as(@user2)
+        get "/questions/#{@question.id}/edit" 
+      end
+      
+      it "redirects to the home page" do 
+        expect(response.status).to eq 302
+        flash_message = "You can only edit your own question." 
+        expect(flash[:alert]).to eq flash_message
+      end 
+    end
+
+    context 'with logged in and owner' do 
+      before do
+        login_as(@user)
+        get "/questions/#{@question.id}/edit" 
+      end
+      
+      it "successfully edits question" do 
+        expect(response.status).to eq 200
+      end 
+    end
+
+  end
 end
