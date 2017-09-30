1699ae81d8c3630eb04964347e8d33fa0aba1d4c
Joshua Torres <torres_306@yahoo.com>
2017-09-30 10:32:56 -0400

    test for no questions to list

--- spec/features/questions/list_questions_spec.rb
+++ spec/features/questions/list_questions_spec.rb
@@ -3,12 +3,13 @@ require 'rails_helper'
 RSpec.feature 'List questions' do 
 
   before do
+
     @question1 = FactoryGirl.create(:question)
     @question2 = FactoryGirl.create(:question)
 
   end
 
-  scenario "A user should see top 10 questions" do
+  scenario "A user should see questions" do
 
     visit root_path
 
@@ -16,6 +17,20 @@ RSpec.feature 'List questions' do
     expect(page).to have_content(@question2.question)
     expect(page).to have_link(@question1.question)
     expect(page).to have_link(@question2.question)
+
+ end
+
+ scenario "A user should not see questions" do
+    Question.delete_all
+
+    visit root_path
+
+    expect(page).not_to have_content(@question1.question)
+    expect(page).not_to have_content(@question2.question)
+    expect(page).not_to have_link(@question1.question)
+    expect(page).not_to have_link(@question2.question)
+
+    expect(page).to have_content("No Questions... please add")
 
  end
 
