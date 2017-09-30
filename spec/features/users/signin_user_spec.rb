bd08cd259e05d62eac78c3634fb6824b8d0f1260
Joshua Torres <torres_306@yahoo.com>
2017-09-30 15:51:34 -0400

    dispay current user when loged in

--- spec/features/users/signin_user_spec.rb
+++ spec/features/users/signin_user_spec.rb
@@ -3,21 +3,22 @@ require 'rails_helper'
 RSpec.feature 'Signup user' do 
 
   before do
-    @user = FactoryGirl.build(:user)
+    @user = FactoryGirl.create(:user)
 
   end
+
 
   scenario "user can sign up with valid creds" do
     visit root_path
     click_link "Sign In"
-    click_link "Sign up"
     fill_in 'Email', with: @user.email
     fill_in 'Password', with: @user.password
-    fill_in 'Password confirmation', with: @user.password_confirmation
-    click_button 'Sign up'
+    click_button 'Log in'
 
-    expect(page).to have_content('Welcome! You have signed up successfully.')
+    expect(page).to have_content('Signed in successfully.')
+    expect(page).to have_content(@user.email)
 
-  end  
+  end
+
 
 end
