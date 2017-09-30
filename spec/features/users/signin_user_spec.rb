0e9446031be451c76bd68fc2d83dc77c78e70911
Joshua Torres <torres_306@yahoo.com>
2017-09-30 15:54:41 -0400

    logout

--- spec/features/users/signin_user_spec.rb
+++ spec/features/users/signin_user_spec.rb
@@ -20,5 +20,17 @@ RSpec.feature 'Signup user' do
 
   end
 
+  scenario "user can sign up with valid creds" do
+    visit root_path
+    click_link "Sign In"
+    fill_in 'Email', with: @user.email
+    fill_in 'Password', with: @user.password
+    click_button 'Log in'
+    click_link "Logout"
+
+    expect(page).to have_content('Signed out successfully.')
+    expect(page).not_to have_content(@user.email)
+
+  end
 
 end
