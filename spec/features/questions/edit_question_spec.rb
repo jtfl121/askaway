f2e900f3edbca940a52135b08705ff4a09f6d97e
Joshua Torres <torres_306@yahoo.com>
2017-09-30 12:14:38 -0400

    add ability to edit questions

---
+++ spec/features/questions/edit_question_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.feature 'Question' do 
+
+  before do
+
+    @question = FactoryGirl.create(:question)
+
+  end
+
+  scenario 'A user edits a question' do
+    visit root_path
+
+    click_link @question.question
+    click_link 'Edit question'
+
+    fill_in "Question", with: "update question"
+
+    click_button "Update question"
+
+    expect(page).to have_content("Question was updated")
+    expect(current_path).to eq(question_path(@question))
+
+  end
+
+  scenario 'A user fails to edit a question' do
+    visit root_path
+
+    click_link @question.question
+    click_link 'Edit question'
+
+    fill_in "Question", with: ""
+
+    click_button "Update question"
+
+    expect(page).to have_content("Question was not updated")
+    expect(current_path).to eq(question_path(@question))
+
+  end
+
+end
