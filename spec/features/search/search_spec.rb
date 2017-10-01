58697850e21dd5e1af408ade1b37b75b527d8f61
Joshua Torres <torres_306@yahoo.com>
2017-10-01 18:35:00 -0400

    add form for search

--- spec/features/search/search_spec.rb
+++ spec/features/search/search_spec.rb
@@ -4,10 +4,10 @@ RSpec.feature 'Search' do
 
     @user = FactoryGirl.create(:user)
 
-    @question = FactoryGirl.create(:question, user: @user, tag_list:"tag1")
+    @question = FactoryGirl.create(:question, question: "Uniq_string_123", user: @user, tag_list:"tag1")
     @question2 = FactoryGirl.create(:question, user: @user, tag_list:"tag2")
     @question3 = FactoryGirl.create(:question, user: @user, tag_list:"tag1, tag3")
-    @question4 = FactoryGirl.create(:question, user: @user, tag_list:"tag1")
+    @question4 = FactoryGirl.create(:question, user: @user, tag_list:"tag4")
 
 
   end
@@ -15,11 +15,27 @@ RSpec.feature 'Search' do
   scenario 'A user can search for questions' do
     visit root_path
 
-    fill_in "Search", with: @question.question
+    fill_in "search_bar", with: @question.question
+
+    click_button "Search"
 
     expect(page).to have_content(@question.question)
     expect(page).not_to have_content(@question2.question)
     expect(page).not_to have_content(@question3.question)
+    expect(page).not_to have_content(@question4.question)
+
+  end  
+
+  scenario 'A user can search questions by tag' do
+    visit root_path
+
+    fill_in "search_bar", with: "tag1"
+
+    click_button "Search"
+
+    expect(page).to have_content(@question.question)
+    expect(page).to have_content(@question3.question)
+    expect(page).not_to have_content(@question2.question)
     expect(page).not_to have_content(@question4.question)
 
   end
