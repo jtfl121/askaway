383ef3a218921129c0aaf59047acd524c1f4be41
Joshua Torres <torres_306@yahoo.com>
2017-10-01 17:57:06 -0400

    add pgsearch

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   root to: 'questions#index'
   get 'tags/:tag', to: 'questions#index', as: :tag
+  # get 'search/:q', to: 'questions#index', as: :q
+  # resources :search, only: [:index]
 
   match '*path', :to => 'application#routing_error', :via => [:get]
 
